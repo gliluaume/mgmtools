126d3e48675e7cf8c9d059b278385186df3a2ddc
guillaume <salicisguillaume@yahoo.fr>
2017-11-14 19:46:47 +0100

    chore(js version): updating js tools

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const commandLineArgs = require('command-line-args')
 const ndf = require('./ndf')
 
 const optionDefinitions = [
-  { name: 'header', alias: 'e', type: Boolean },
-  { name: 'src', type: String, defaultOption: true }
+  { name: 'header', alias: 'e', type: Boolean, defaultOption: true },
+  { name: 'src', alias: 's', type: String }
 ]
 
 const options = commandLineArgs(optionDefinitions)
 
 const actual= ndf.get(options.src)
 
-if (options.header)
+if (options.header) {
   ndf.header()
+}
 
 ndf.write(actual)
--- ndf.js
+++ ndf.js
@@ -2,26 +2,50 @@ const moment = require('moment')
 const path = require('path')
 const fs = require('fs')
 
+const sep = ';'
+
 function get(dirname) {
   return fs.readdirSync(dirname)
-  .filter(filename => filename.match(/^[0-9]{8}-ndf-repas-[0-9]{3,4}.jpg$/))
-  .sort()
+  .filter(filename => filename.match(/^[0-9]{8}-[a-z]*-[a-zA-Z]*-[0-9]{3,7}.[a-z]{3}$/))
   .map((filename) => {
-    const splitted = path.basename(filename, '.jpg').split(/[-.]/)
+    const splitted = path.parse(filename).name.split(/[-.]/)
     return {
-      date: moment(splitted[0], 'YYYYMMDD').format('DD/MM/YYYY'),
-      type: splitted[2],
+      date: moment(splitted[0], 'YYYYMMDD'),
+      type: splitted[1],
+      title: splitted[2],
       amount: parseInt(splitted[3]) / 100
     }
   })
+  .sort((da, db) => da.date - db.date)
+  .map(data => ({
+      date: data.date.format('DD/MM/YYYY'),
+      type: data.type,
+      title: data.title,
+      amount: data.amount
+    }))
 }
 
-function header() {
-  process.stdout.write('date;type;amount\n')
+function header(culture) {
+  const cult = culture || 'fr'
+  const heads = {
+    'en': [
+      'date',
+      'type',
+      'title',
+      'amount'
+      ],
+    'fr': [
+      'date',
+      'type',
+      'titre',
+      'montant'
+      ]
+  }
+  process.stdout.write(`${heads[cult].join(sep)}\n`)
 }
 
 function write(notes) {
-  notes.forEach(note => process.stdout.write(`${note.date};${note.type};${note.amount}\n`))
+  notes.forEach(note => process.stdout.write(`${note.date}${sep}${note.type}${sep}${note.title}${sep}${note.amount}\n`))
 }
 
 module.exports = {
